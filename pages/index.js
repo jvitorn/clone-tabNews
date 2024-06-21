3dc1520dafc798b86cfedef10720320a8940ad27
João Vitor Novais <41808876+jvitorn@users.noreply.github.com>
2024-06-21 22:46:56 +0000

    adicionando um recado motivacional na home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
 function Home() {
+    const containerStyle = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+    };
+
+    const imgStyle = {
+        maxWidth: '100%',
+        height: 'auto',
+    };
     return <>
-        <h1>Curso.dev</h1>
-        <p>Olá Devs, esse é o primeiro teste de deploy do clone tabnews</p>
+        <div style={containerStyle}>
+            <h1>Olá, Devs!</h1>
+            <p>
+                Se você chegou até aqui, saiba que é uma pessoa fantástica. Não desista, pois você está no caminho para se tornar uma pessoa e um profissional cada vez mais incrível. Mesmo que a 'Síndrome do Impostor' apareça em algum momento, lembre-se da sua jornada e dos passos que você percorreu e continua a percorrer diariamente. Continue acreditando em si mesmo!
+            </p>
+            <figure>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmM14c-Guv938uehyMw2TW6WX33t7vfxrnYg&s" alt="Imagem Motivacional" style={imgStyle} />
+                <figcaption>Continue acreditando em si mesmo!</figcaption>
+            </figure>
+        </div>
     </>
 }
 
